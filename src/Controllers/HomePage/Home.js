133ad095fed5de1a5b2925157bf62f13e12bd2d1
kashishy24 <kashishy2403.com>
2025-09-23 15:58:39 +0530

    add the get plan and actual qty api and total and good part api

--- src/Controllers/HomePage/Home.js
+++ src/Controllers/HomePage/Home.js
@@ -36,4 +36,66 @@ router.get("/plantOEE", async (req, res) => {
   }
 });
 
+//Get the Plan and actual qty acc to filter
+router.get("/GetPlanActualQty", async (req, res) => {
+  try {
+    const { mode, startDate, endDate } = req.query;
+
+    // Connect to DB
+    const pool = await sql.connect();
+
+    // Call stored procedure
+    const result = await pool
+      .request()
+      .input("Mode", sql.VarChar(20), mode || "SHIFT")  // default SHIFT
+      .input("StartDate", sql.Date, startDate || null)
+      .input("EndDate", sql.Date, endDate || null)
+      .execute("sp_Dashboard_Get_plan_ActualQty");   // or sp_Get_Plant_OEE if that's the correct name
+
+    res.json({
+      success: true,
+      data: result.recordset,
+    });
+  } catch (error) {
+    console.error("Error fetching plan Actual Qty data:", error);
+    res.status(500).json({
+      success: false,
+      message: "Error fetching plan Actual Qty data",
+      error: error.message,
+    });
+  }
+});
+
+
+//Get the Ok and Total qty acc to filter
+router.get("/GetOKTotalQty", async (req, res) => {
+  try {
+    const { mode, startDate, endDate } = req.query;
+
+    // Connect to DB
+    const pool = await sql.connect();
+
+    // Call stored procedure
+    const result = await pool
+      .request()
+      .input("Mode", sql.VarChar(20), mode || "SHIFT")  // default SHIFT
+      .input("StartDate", sql.Date, startDate || null)
+      .input("EndDate", sql.Date, endDate || null)
+      .execute("sp_Dashboard_Get_OK_TotalQty");   // or sp_Get_Plant_OEE if that's the correct name
+
+    res.json({
+      success: true,
+      data: result.recordset,
+    });
+  } catch (error) {
+    console.error("Error fetching OK and Total Qty data:", error);
+    res.status(500).json({
+      success: false,
+      message: "Error fetching OK and Total  Qty data",
+      error: error.message,
+    });
+  }
+});
+
+
 module.exports = router;
