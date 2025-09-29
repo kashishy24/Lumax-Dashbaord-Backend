bda9e7f74f32f8f1261d358d61bc845e3ef24039
kashishy24 <kashishy2403.com>
2025-09-29 11:41:38 +0530

    add the api to show losses

--- src/Controllers/Performance/machineWiseData.js
+++ src/Controllers/Performance/machineWiseData.js
@@ -64,4 +64,198 @@ router.get("/machineTimes", async (req, res) => {
     });
   }
 });
+
+
+//Get the loss Name
+
+// GET Loss Names
+router.get("/lossname", async (req, res) => {
+  try {
+  
+    // Run Stored Procedure
+    const pool = await sql.connect();
+    const result = await pool
+      .request()
+      .execute("sp_Dashbaord_get_LossName");
+
+    res.json({
+      success: true,
+      data: result.recordset,
+    });
+  } catch (error) {
+    console.error("Error fetching LossName Data:", error);
+    res.status(500).json({
+      success: false,
+      message: "Error fetching LossName data",
+      error: error.message,
+    });
+  }
+});
+
+//Get subloss Names
+
+// GET Loss Names
+router.get("/Sublossname", async (req, res) => {
+  try {
+    const { LossID } = req.query; // pass LossID as query param if needed
+
+    // Run Stored Procedure
+    const pool = await sql.connect();
+    const result = await pool
+      .request()
+      .input("LossID", sql.Int, LossID || null) // if your SP takes LossID
+      .execute("sp_dashbaord_GetSubLossBy_LossID");
+
+    res.json({
+      success: true,
+      data: result.recordset,
+    });
+  } catch (error) {
+    console.error("Error fetching Sub LossName Data:", error);
+    res.status(500).json({
+      success: false,
+      message: "Error fetching Sub LossName  data",
+      error: error.message,
+    });
+  }
+});
+
+//Get the All Losses and selected loss duration and occurence by hourly (Current Shift)
+router.get("/hourlylossForShift", async (req, res) => {
+  try {
+    const { EquipmentKey, IncludeZeroHours, LossID, CountType } = req.query;
+
+    const pool = await sql.connect();
+    const result = await pool
+      .request()
+      .input("EquipmentKey", sql.NVarChar(100), EquipmentKey)        // required
+      .input("IncludeZeroHours", sql.Bit, IncludeZeroHours || 1)     // default = 1
+      .input("LossID", sql.Int, LossID || null)                      // optional
+      .input("CountType", sql.VarChar(10), CountType || "overlap")   // default = 'overlap'
+      .execute("SP_GetHourlyLossAndOccurrences_ForEquipment_CurrentShift_New");
+
+    res.json({
+      success: true,
+      data: result.recordset,
+    });
+  } catch (error) {
+    console.error("Error fetching Hourly Loss Data:", error);
+    res.status(500).json({
+      success: false,
+      message: "Error fetching Hourly Loss data",
+      error: error.message,
+    });
+  }
+});
+
+
+//Get the All Losses and selected loss duration and occurence by day,week,month,dynamic date selection
+router.post("/lossesForDayWeekMonthDates", async (req, res) => {
+  try {
+    const {
+      EquipmentKey,
+      Mode,
+      IncludeZeroPeriods,
+      LossID,
+      CountType,
+      RefDate,
+      StartDate,
+      EndDate,
+    } = req.body;
+
+    const pool = await sql.connect();
+    const result = await pool
+      .request()
+      .input("EquipmentKey", sql.NVarChar(100), EquipmentKey)                 // required
+      .input("Mode", sql.VarChar(10), Mode || "day")
+      .input("IncludeZeroPeriods", sql.Bit, IncludeZeroPeriods ?? 1)
+      .input("LossID", sql.Int, LossID ?? null)
+      .input("CountType", sql.VarChar(10), CountType || "overlap")
+      .input("RefDate", sql.Date, RefDate ?? null)
+      .input("StartDate", sql.Date, StartDate ?? null)
+      .input("EndDate", sql.Date, EndDate ?? null)
+      .execute("SP_GetLosses_ByMode_WithOccurrences_Duration");
+
+    res.json({
+      success: true,
+      data: result.recordset,
+    });
+  } catch (error) {
+    console.error("Error fetching Losses Data:", error);
+    res.status(500).json({
+      success: false,
+      message: "Error fetching Losses data",
+      error: error.message,
+    });
+  }
+});
+
+//Get the All Losses  duration and occurence by hourly (Current Shift)
+router.get("/hourlyAlllossForShift", async (req, res) => {
+  try {
+    const { EquipmentKey, IncludeZeroHours, LossID, CountType } = req.query;
+
+    const pool = await sql.connect();
+    const result = await pool
+      .request()
+      .input("EquipmentKey", sql.NVarChar(100), EquipmentKey)        // required
+      
+      .input("LossID", sql.Int, LossID || null)                      // optional
+   // default = 'overlap'
+      .execute("SP_Dashbaord_Get_ALL_Shift_LossSummary");
+
+    res.json({
+      success: true,
+      data: result.recordset,
+    });
+  } catch (error) {
+    console.error("Error fetching Hourly Lossess Data:", error);
+    res.status(500).json({
+      success: false,
+      message: "Error fetching Hourly Lossess data",
+      error: error.message,
+    });
+  }
+});
+
+//Get the All Losses  duration and occurence by day,week,month,dynamic date selection
+router.post("/lossesForDayWeekMonthDates", async (req, res) => {
+  try {
+    const {
+      EquipmentKey,
+      Mode,
+      IncludeZeroPeriods,
+      LossID,
+      CountType,
+      RefDate,
+      StartDate,
+      EndDate,
+    } = req.body;
+
+    const pool = await sql.connect();
+    const result = await pool
+      .request()
+      .input("EquipmentKey", sql.NVarChar(100), EquipmentKey)                 // required
+      .input("Mode", sql.VarChar(10), Mode || "day")
+      .input("IncludeZeroPeriods", sql.Bit, IncludeZeroPeriods ?? 1)
+      .input("LossID", sql.Int, LossID ?? null)
+      .input("CountType", sql.VarChar(10), CountType || "overlap")
+      .input("RefDate", sql.Date, RefDate ?? null)
+      .input("StartDate", sql.Date, StartDate ?? null)
+      .input("EndDate", sql.Date, EndDate ?? null)
+      .execute("SP_Dashbaord_Get_ALL_DWMDates_LossSummary_ByMode1");
+
+    res.json({
+      success: true,
+      data: result.recordset,
+    });
+  } catch (error) {
+    console.error("Error fetching Losses Data:", error);
+    res.status(500).json({
+      success: false,
+      message: "Error fetching Losses data",
+      error: error.message,
+    });
+  }
+});
 module.exports = router;
